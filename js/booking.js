ced57c14e176e4ad006e46304ae3326af05fc903
aranis121 <ar.pinterest121@gmail.com>
2021-01-30 17:27:09 +0600

    change code

--- js/booking.js
+++ js/booking.js
@@ -1,4 +1,5 @@
-function firstTicketPrice(increase){
+
+function handleTicketPrice(increase){
     const firstClassInput = document.getElementById("first-class-input");
     const firstInputNumber = parseInt(firstClassInput.value);
     let firstNewInput = firstInputNumber;
@@ -49,3 +50,47 @@ function calculateSubTotal(){
     const allTotalCost = subTotal + tax;
     document.getElementById("all-total").innerText = "$" + allTotalCost
 }
+
+function bookNow() {
+    // traveling place
+    document.getElementById('display-from').innerText = document.getElementById('fling-from').value;
+    document.getElementById('display-to').innerText = document.getElementById('fling-to').value;
+
+    // traveling date
+    document.getElementById('display-departure-date').innerText = document.getElementById('departure').value;
+    document.getElementById('display-return-date').innerText = document.getElementById('return').value;
+
+    // first class information
+    document.getElementById('firsts-class').innerText = document.getElementById('first-class-input').value;
+    document.getElementById('fast-class-price').innerText = document.getElementById('first-class-input').value * 150;
+
+    // economy information
+    document.getElementById('second-class').innerText = document.getElementById('economy-class-input').value;
+    document.getElementById('second-class-price').innerText = document.getElementById('economy-class-input').value * 100;
+
+    // subtotal information
+    document.getElementById('subtotal-price').innerText = document.getElementById('sub-total').innerText;
+
+    // vat information
+    document.getElementById('vat').innerText = document.getElementById('tax-amount').innerText;
+
+    // total information
+    document.getElementById('total-count').innerText = parseInt(getElementById('first-class-input').value) + parseInt(getElementById('economy-class-input').value);
+    document.getElementById('total-price').innerText = getElementById('all-total').innerText;
+}
+// get element id 
+function getElementById(id) {
+    return document.getElementById(id);
+}
+
+
+
+
+
+
+
+
+
+
+
+
